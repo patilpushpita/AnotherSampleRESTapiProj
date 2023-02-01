36a8ff8765ae1e86f742031a76447a60bfd875fa
PATIL14 <pushpita.patil@lexisnexis.com>
2023-02-01 09:08:52 -0500

    Initial commit

---
+++ src/main/java/org/example/Server.java
@@ -0,0 +1,64 @@
+package org.example;
+
+import java.io.*;
+import java.net.*;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+import com.sun.net.httpserver.*;
+
+public class Server {
+    public void main(String[] args) throws Exception {
+        var port = new InetSocketAddress(8000);
+
+        var server = HttpServer.create(port, 0);
+
+        server.createContext("/home", new FrontendHandler());
+        server.createContext("/randomNumber", new RandomNumberHandler());
+
+        server.setExecutor(null);
+
+        server.start();
+    }
+
+    static class FrontendHandler implements HttpHandler {
+        @Override
+        public void handle(HttpExchange request) throws IOException {
+            var appPath = Paths.get("test.html");
+
+            var app = new String(Files.readAllBytes(appPath));
+
+            var appBytesLength = app.getBytes().length;
+
+            request.sendResponseHeaders(200, appBytesLength);
+
+            var body = request.getResponseBody();
+
+            body.write(app.getBytes());
+
+            body.close();
+
+        }
+    }
+
+    class RandomNumberHandler implements HttpHandler {
+        @Override
+        public void handle(HttpExchange request) throws IOException {
+            var randomNumber = (int) (Math.random() * 100);
+
+            var response = Integer.toString(randomNumber);
+
+            var responseLength = response.getBytes().length;
+
+            request.sendResponseHeaders(200, responseLength);
+
+            var body = request.getResponseBody();
+
+            body.write(response.getBytes());
+
+            body.close();
+        }
+    }
+
+
+}
